54e0ff5c0cab92fc874ce1aa53f5ad8bac92289e
unknown <vodyanickij7@gmail.com>
2016-05-20 00:15:40 +0300

    d

---
+++ bd.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');   
+
+//function connection to the bd
+function connect(){
+    $db= mysql_connect(DB_HOSTNAME,DB_USERNAME,DB_PASSWORD)
+    or die ("<p>Ошибка подключения к базе данных " .mysql_error()."</p>");
+    mysql_select_db("stud")
+    or die ("<p>Ошибка выбора базы данных " .mysql_error(). "</p>");
+}
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../style/style.css" />
+    <title>Близнюковский РЦПМД</title>
+</head>
+<body>
+    <header>
+        <div id="logo">  </div>
+            <ul id="navbar">
+                <li><a href="#">Главная</a></li>
+                <li><a href="#">Новости</a></li>
+                <li><a href="#">Контакты</a></li>
+                <li><a href="#">О нас</a></li>
+                <li><a href="#">Регистрация</a></li>
+                <li><a href="#">Авторизация</a></li>
+            </ul>
+    </header>
+
+    <footer>
+        <div id='social'>
+        </div>
+        <div id='right'>Все права защищены &copy <?=date('Y')?>
+        </div>
+    </footer>
+
+</body>
+</html>
