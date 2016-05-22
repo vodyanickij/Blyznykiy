bab4a018c9f3dd216292f3eb562cf3f1b7b05c92
unknown <sasha-vodyanickij@yandex.ru>
2016-05-22 16:54:04 +0300

    'добавил'

--- index.php
+++ index.php
@@ -10,16 +10,16 @@
         <div id="logo">  </div>
             <ul id="navbar">
                 <li><a href="#">Главная</a></li>
-                <li><a href="#">Новости</a></li>
+                <li><a href="#">Врачи</a></li>
+                <li><a href="#">Запись</a></li>
                 <li><a href="#">Контакты</a></li>
-                <li><a href="#">О нас</a></li>
                 <li><a href="#">Регистрация</a></li>
                 <li><a href="#">Авторизация</a></li>
             </ul>
     </header>
     
     <div id='wrapper'>
-        
+        <h1>Близнюковский РЦПМД</h1>
     </div>
 
     <footer>
